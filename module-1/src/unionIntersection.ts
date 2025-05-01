b403f32a61ee0bb03bfa97e04e002993f3a30ce5
arasruislam <arasru01@gmail.com>
2025-05-01 07:15:50 +0600

    add TypeScript implementations for union and intersection types

---
+++ module-1/src/unionIntersection.ts
@@ -0,0 +1,36 @@
+{
+   // Union types
+   type FrontendDevloper = "Level 1" | "Level 2"; // string literal type
+   type FullstackDevloper = "frontend" | "exprt";
+
+   type Developer = FrontendDevloper | FullstackDevloper;
+
+   const newDeveloper: FrontendDevloper = "Level 1";
+
+   type User = {
+      name: string;
+      email: string;
+      gender: "male" | "female";
+      bloodGroup: "A+" | "A-" | "B+" | "B-" | "O+" | "O-" | "AB+" | "AB-";
+   };
+
+   const user1: User = {
+      name: "John Doe",
+      email: "o4o9v@example.com",
+      gender: "male",
+      bloodGroup: "A+",
+   };
+
+
+   // Intersection types
+   type FrontendDeveloper = {
+      skills: string[];
+      designation: "frontend developer";
+   }
+   type BackendDeveloper = {
+      skills: string[];
+      designation: "backend developer";
+   }
+   type FullstackDeveloper = FrontendDeveloper & BackendDeveloper;
+   
+}
