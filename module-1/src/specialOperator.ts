7b2813227e74654cbce96eebb33924aceafcc1a3
arasruislam <arasru01@gmail.com>
2025-05-01 07:35:14 +0600

    fix: update age variable and enhance voting logic with additional examples of nullish coalescing and optional chaining

--- module-1/src/specialOperator.ts
+++ module-1/src/specialOperator.ts
@@ -1,21 +1,46 @@
 {
    // ternary Operator || optional chaining | nullish coalescing
 
-   const age: number = 15;
+   const age: number = 18;
 
-   if(age >= 18) {
+   if (age >= 18) {
       console.log("You can vote");
    } else {
       console.log("You can't vote");
    }
-   
 
+   const isAdult = age >= 18 ? "adult" : "not adult";
+   console.log({ isAdult });
 
+   // nullish coalescing operator
+   // null / unidefined ---> decision making
+   const isAuthenticated = "";
+   const result1 = isAuthenticated ?? "Guest";
+   const result2 = isAuthenticated ? isAuthenticated : "Guest";
+   console.log({ result1, result2 });
 
+   // Optional chaining
+   type User = {
+      name: string;
+      address: {
+         street: string;
+         city: string;
+         road: string;
+         presentAddress: string;
+         permanentAddress?: string;
+      }
+   }
 
-
-
-
-
-
+   const user: User = {
+      name: "John Doe",
+      address: {
+         street: "123 Main St",
+         city: "New York",
+         road: "Broadway",
+         presentAddress: "123 Main St",
+         // permanentAddress: "123 Main St",
+      }
+   }
+   const permanentAddress = user.address?.permanentAddress ?? "No permanent address";
+   console.log({ permanentAddress });
 }
