651f4e026afd8452b808b8d581a3c96afaf0156b
arasruislam <arasru01@gmail.com>
2025-05-05 16:53:03 +0600

    add: implement makeSound method in Animal class and instantiate dog and cat objects

--- module-3/src/classObject.ts
+++ module-3/src/classObject.ts
@@ -9,8 +9,15 @@
          this.sound = sound;
          this.species = species;
       }
+
+      makeSound() {
+         console.log(`the ${this.name} says ${this.sound}`);
+      }
    }
 
    const dog = new Animal("German shepard", "dog", "ghew ghew");
-   
+   const cat = new Animal("partian", "cat", "meaw meaw");
+
+   dog.makeSound();
+   cat.makeSound();
 }
