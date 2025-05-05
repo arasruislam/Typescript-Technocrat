877be7d107546643738e4b8f3fed362547f8f4cf
arasruislam <arasru01@gmail.com>
2025-05-05 16:33:23 +0600

    add: implement generics using Array<T> syntax for roll numbers, mentors, and boolean arrays

---
+++ module-2/src/generics.ts
@@ -0,0 +1,23 @@
+{
+   // Generic
+
+   // const rollNumbers: number[] = [2, 3, 4, 5, 6];
+   const rollNumbers: Array<number> = [2, 3, 4, 5, 6];
+   
+
+
+   // const mentors: string[] = ["Goku", "Vegeta", "Gohan"];
+   const mentors: Array<string> = ["Goku", "Vegeta", "Gohan"];
+
+
+
+
+   // const boolArray: boolean[] = [true, false, true, false, true];
+   const boolArray: Array<boolean> = [true, false, true, false, true];
+
+
+
+
+
+
+}
