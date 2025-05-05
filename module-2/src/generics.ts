bf4245c9fa7db93a727c13f414266baedeffdc9c
arasruislam <arasru01@gmail.com>
2025-05-05 16:37:40 +0600

    refactor: use generic type for roll numbers in generics.ts

--- module-2/src/generics.ts
+++ module-2/src/generics.ts
@@ -1,14 +1,13 @@
 {
    // Generic
+   type genericType<T> = Array<T>;
 
    // const rollNumbers: number[] = [2, 3, 4, 5, 6];
-   const rollNumbers: Array<number> = [2, 3, 4, 5, 6];
+   const rollNumbers: genericType<number> = [2, 3, 4, 5, 6];
    
-
 
    // const mentors: string[] = ["Goku", "Vegeta", "Gohan"];
    const mentors: Array<string> = ["Goku", "Vegeta", "Gohan"];
-
 
 
 
@@ -16,7 +15,7 @@
    const boolArray: Array<boolean> = [true, false, true, false, true];
 
 
-
+   
 
 
 
