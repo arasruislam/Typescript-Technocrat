9ef545865396413021eff8d6c29d53352f9d4486
arasruislam <arasru01@gmail.com>
2025-05-06 17:39:38 +0600

    refactor: clean up whitespace and improve formatting in generics.ts

--- module-2/src/generics.ts
+++ module-2/src/generics.ts
@@ -4,19 +4,31 @@
 
    // const rollNumbers: number[] = [2, 3, 4, 5, 6];
    const rollNumbers: genericType<number> = [2, 3, 4, 5, 6];
-   
 
    // const mentors: string[] = ["Goku", "Vegeta", "Gohan"];
    const mentors: Array<string> = ["Goku", "Vegeta", "Gohan"];
 
-
-
    // const boolArray: boolean[] = [true, false, true, false, true];
    const boolArray: Array<boolean> = [true, false, true, false, true];
 
+   const user: genericType<{ name: string; age: number }> = [
+      {
+         name: "mazba",
+         age: 100,
+      },
+      {
+         name: "asru",
+         age: 110,
+      },
+   ];
 
-   
+   // generic tuple
+   // special array
+   type genericTuple<X, Y> = [X, Y];
+   const people: genericTuple<string, string> = ["m", "s"];
 
-
-
+   const userWithId: [number, { name: string; email: string }] = [
+      1234,
+      { name: "person", email: "as@gmail.com" },
+   ];
 }
